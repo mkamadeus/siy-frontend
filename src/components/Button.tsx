06bf2c4ae19f0f5b3efac8cec4cd09dba7a67ebd
Annisa Rahim <13518089@std.stei.itb.ac.id>
2021-03-04 11:27:20 +0700

    fix button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ const Button: React.FC = () => {
   return (
     <button
       type="submit"
-      className="w-1/2 flex items-center justify-center rounded-md bg-blue-500 text-white hover:shadow-lg border-gray-200 focus:bg-blue-600 m-3"
+      className="w-full flex items-center justify-center rounded-md bg-blue-500 text-white py-2 px-4 hover:shadow-lg border-gray-200 focus:bg-blue-600"
     >
       Upload
     </button>
