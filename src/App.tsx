6e4a0b1ffe505a4b9756f44a804b7ed3302a1470
Izharulhaq <izharhaq00@gmail.com>
2021-04-08 21:59:45 +0700

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ import TeacherPage from '~/pages/TeacherPage';
 import Navbar from './components/Navbar';
 import TranscriptPage from './pages/TranscriptPage';
 import PeerAssesmentForm from './pages/PeerAssesmentForm';
-import LoDetailPage from './pages/LoDetailPage'
-import AdminCoursePage from './pages/admin/course/AdminCoursePage';
+// import AdminCoursePage from './pages/admin/course/AdminCoursePage';
 import AdminCourseCreatePage from './pages/admin/course/AdminCourseCreatePage';
 import AdminIndexPage from './pages/admin/AdminIndexPage';
 import AdminCourseEditPage from './pages/admin/course/AdminCourseEditPage';
+import LoDetailPage from './pages/LoDetailPage';
+import AdminLecturePage from './pages/admin/lecture/AdminLecturePage';
+import AdminLectureCreatePage from './pages/admin/lecture/AdminLectureCreatePage';
+import AdminLectureEditPage from './pages/admin/lecture/AdminLectureEditPage';
+import AdminLecturePreviewPage from './pages/admin/lecture/AdminLecturePreviewPage';
+import AdminCoursePreviewPage from './pages/admin/course/AdminCoursePreviewPage';
 
 const queryClient = new QueryClient();
 
@@ -31,9 +36,18 @@ const App: React.FC = () => {
         <PeerAssesmentForm path="/peer-assessment-form" />
 
         <AdminIndexPage path="/admin" />
-        <AdminCoursePage path="/admin/course" />
+
+        {/* ADMIN COURSE */}
+        {/* TODO: <AdminCoursePage path="/admin/course" /> */}
+        <AdminCoursePreviewPage path="/admin/course/:id" />
         <AdminCourseCreatePage path="/admin/course/create" />
         <AdminCourseEditPage path="/admin/course/edit/:id" />
+
+        {/* ADMIN LECTURE */}
+        <AdminLecturePage path="/admin/course" />
+        <AdminLecturePreviewPage path="/admin/course/:id" />
+        <AdminLectureEditPage path="/admin/course/edit/:id" />
+        <AdminLectureCreatePage path="/admin/course/create" />
       </Router>
     </QueryClientProvider>
   );
--- src/api/Teacher.ts
+++ src/api/Teacher.ts
@@ -1,18 +1,16 @@
 import axios from 'axios';
-// import { env } from '~/env';
-// import { CourseGrade } from '~/model/Course';
 import { Teacher } from '~/model/Teacher';
-// import { dummyRequest } from './Dummy';
 import { BaseInstance } from './Base';
-import { Course } from '~/model/Course';
 
-export const getCourseData = async (): Promise<Course[]> => {
-  const courses = await BaseInstance.get<Course[]>(`/courses/`);
-  return courses.data;
+export const getTeacherData = async (): Promise<Teacher[]> => {
+  const res = await axios.get<Teacher[]>(
+    `${import.meta.env.SNOWPACK_PUBLIC_API_URL}/teachers`
+  );
+  return res.data;
 };
 
 export const getTeacherById = async (id: number): Promise<Teacher> => {
-  const teachers = await BaseInstance.get<Teacher>(`/teacher/${id}`);
+  const teachers = await BaseInstance.get<Teacher>(`/teachers/${id}`);
   return teachers.data;
 };
 
@@ -25,6 +23,9 @@ export const updateTeacher = async (
   id: number,
   teacherData: Partial<Teacher>
 ): Promise<Teacher> => {
-  const teachers = await BaseInstance.put<Teacher>(`/teachers/${id}`, teacherData);
+  const teachers = await BaseInstance.put<Teacher>(
+    `/teachers/${id}`,
+    teacherData
+  );
   return teachers.data;
 };
