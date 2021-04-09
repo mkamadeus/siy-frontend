cb6c09292c309cc8c3cf817721900c14c6c95fc4
Izharulhaq <izharhaq00@gmail.com>
2021-04-10 00:59:43 +0700

    Post form (temp)

--- src/api/Lecture.ts
+++ src/api/Lecture.ts
@@ -1,10 +1,33 @@
 import { Lecture } from '~/model/Lecture';
+import { BaseInstance } from './Base';
 
 export const getAllLectures = async (): Promise<Lecture[]> => {
-  return [];
+  const lectures = await BaseInstance.get<Lecture[]>('/lectures');
+  return lectures.data;
 };
 
-// TODO: implementation
-export const createLecture = async (lecture: Lecture): Promise<void> => {
-  return;
+export const getLectureById = async (id: number): Promise<Lecture> => {
+  const lecture = await BaseInstance.get<Lecture>(`/lectures/${id}`);
+  return lecture.data;
+}
+
+export const getLectureByYearSemester = async (
+  year: number,
+  semester: number
+  ): Promise<Lecture[]> => {
+    const lectures = await BaseInstance.get<Lecture[]>(`/lectures/year/${year}/${semester}`);
+    return lectures.data;
+}
+
+export const createLecture = async (lectureData: Lecture): Promise<Lecture> => {
+  const lecture = await BaseInstance.post<Lecture>('/lectures', lectureData);
+  return lecture.data;
 };
+
+export const updateLecture = async (
+  id: number,
+  lectureData: Partial<Lecture>
+): Promise<Lecture> => {
+  const lecture = await BaseInstance.put<Lecture>(`/lectures/${id}`, lectureData);
+  return lecture.data;
+}
--- src/pages/TeacherPage.tsx
+++ src/pages/TeacherPage.tsx
@@ -6,12 +6,72 @@ import InputLO from '../components/page/LOInput';
 import LoadingPage from './common/LoadingPage';
 import { getAllCourses } from '~/api/Course';
 import { useQuery } from 'react-query';
+import { createLecture } from '~/api/Lecture';
+import { Lecture } from '~/model/Lecture';
 
 const TeacherPage: React.FC<RouteComponentProps> = (
     props: RouteComponentProps
 ) => {
     const { data, error, isLoading } = useQuery('courseData', getAllCourses);
     const { register, handleSubmit } = useForm();
+
+
+    const courseIDValue = () => {
+        let value = 0;
+        const elmt = document.getElementsByName('course')[0];
+        if (elmt instanceof HTMLSelectElement) {
+            value = Number(data?.filter((d) => {
+                return d.code == elmt.value;
+            })[0].id);
+        }
+        
+        return value;
+    }
+
+    const semesterValue = () => {
+        let value = 0;
+        const elmt = document.getElementsByName('course')[0];
+        if (elmt instanceof HTMLSelectElement) {
+            value = Number(elmt.value);
+        }
+        
+        return value;
+    }
+
+    const yearValue = () => {
+        let value = 0;
+        const elmt = document.getElementsByName('year')[0];
+        if (elmt instanceof HTMLInputElement) {
+            value = Number(elmt.value);
+        }
+        
+        return value;
+    }
+
+    const portofolioValue = () => {
+        let value = 0;
+        const elmt = document.getElementsByName('year')[0];
+        if (elmt instanceof HTMLInputElement) {
+            value = Number(elmt.value);
+        }
+        
+        return value;
+    }
+
+    const LOValue = (name: string) => {
+        const value : number[] = [];
+        const LOList = ['LO-A', 'LO-B', 'LO-C', 'LO-D', 'LO-E', 'LO-F', 'LO-G'];
+        
+        LOList.forEach((lo) => {
+            const elmt = document.getElementsByName(name + '-' + lo)[0];
+            if (elmt instanceof HTMLInputElement) {
+                value.push(Number(elmt.value));
+            }
+        });
+
+        return value;
+    }
+
 
     const isLOValid = (title: string, LOList: string[]) => {
         let value = 0;
@@ -55,13 +115,77 @@ const TeacherPage: React.FC<RouteComponentProps> = (
         }
     };
 
+
+    const postForm = () => {
+        const courseID = courseIDValue();
+        const semester = semesterValue();
+        const year = yearValue();
+        const portofolio = portofolioValue();
+        const LoUAS = LOValue('UAS');
+        const LoUTS = LOValue('UTS');
+        const LoPraktikum = LOValue('Praktikum');
+        const LoKuis = LOValue('Kuis');
+        const LoLatihan = LOValue('Latihan');
+        const LoKMT = LOValue('KMT');
+
+        console.log(portofolio);
+
+        createLecture({
+            courseId: courseID,
+            semester: semester,
+            year: year,
+            loAKMTWeight: LoKMT[0],
+            loBKMTWeight: LoKMT[1],
+            loCKMTWeight: LoKMT[2],
+            loDKMTWeight: LoKMT[3],
+            loEKMTWeight: LoKMT[4],
+            loFKMTWeight: LoKMT[5],
+            loGKMTWeight: LoKMT[6],
+            loAFinalWeight: LoUAS[0],
+            loBFinalWeight: LoUAS[1],
+            loCFinalWeight: LoUAS[2],
+            loDFinalWeight: LoUAS[3],
+            loEFinalWeight: LoUAS[4],
+            loFFinalWeight: LoUAS[5],
+            loGFinalWeight: LoUAS[6],
+            loAMidWeight: LoUTS[0],
+            loBMidWeight: LoUTS[1],
+            loCMidWeight: LoUTS[2],
+            loDMidWeight: LoUTS[3],
+            loEMidWeight: LoUTS[4],
+            loFMidWeight: LoUTS[5],
+            loGMidWeight: LoUTS[6],
+            loAPracticumWeight: LoPraktikum[0],
+            loBPracticumWeight: LoPraktikum[1],
+            loCPracticumWeight: LoPraktikum[2],
+            loDPracticumWeight: LoPraktikum[3],
+            loEPracticumWeight: LoPraktikum[4],
+            loFPracticumWeight: LoPraktikum[5],
+            loGPracticumWeight: LoPraktikum[6],
+            loAQuizWeight: LoKuis[0],
+            loBQuizWeight: LoKuis[1],
+            loCQuizWeight: LoKuis[2],
+            loDQuizWeight: LoKuis[3],
+            loEQuizWeight: LoKuis[4],
+            loFQuizWeight: LoKuis[5],
+            loGQuizWeight: LoKuis[6],
+            loAHomeworkWeight: LoLatihan[0],
+            loBHomeworkWeight: LoLatihan[1],
+            loCHomeworkWeight: LoLatihan[2],
+            loDHomeworkWeight: LoLatihan[3],
+            loEHomeworkWeight: LoLatihan[4],
+            loFHomeworkWeight: LoLatihan[5],
+            loGHomeworkWeight: LoLatihan[6]
+        } as Lecture);
+    };
+
+
     const onSubmit = (values: unknown) => {
         const LOValid = isAllLOValid();
         const FileValid = isFileValid();
         
         if (LOValid && FileValid) {
-            alert('Input berhasil disimpan');
-            // TODO : Post form
+            postForm();
         }
         else {
             if (!FileValid) {
@@ -155,6 +279,7 @@ const TeacherPage: React.FC<RouteComponentProps> = (
                     <input
                         className="container p-3 rounded-md border-b bg-gray-200 mb-3"
                         type="text"
+                        name="portofolio"
                         placeholder="ex. 0.93"
                         pattern="^\d\.?\d{0,2}$"
                     ></input>
