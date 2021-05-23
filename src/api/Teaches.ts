eecdc91d56c81f834b1b5cf06bc650f76f879b92
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-05-24 01:35:18 +0700

    hotfix authcontext gone

--- src/api/Teaches.ts
+++ src/api/Teaches.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { Teaches } from '~/model/Teaches';
 import { BaseInstance } from './Base';
 
--- src/pages/questionnaire/QuestionnairePage.tsx
+++ src/pages/questionnaire/QuestionnairePage.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 
-
 import { useQuery } from 'react-query';
 import LoadingPage from '../common/LoadingPage';
-import QuestionnaireTable from '~/components/page/questionnaire/questionnaireTable'
+import QuestionnaireTable from '~/components/page/questionnaire/questionnaireTable';
 import { getLectureById } from '~/api/Lecture';
 
 type RouteProps = {
   lId: number;
   gId: number;
-}
+};
 
 const QuestionnairePage: React.FC<RouteComponentProps> = (
   props: RouteComponentProps<RouteProps>
 ) => {
+  // const {student} = useContext(AuthContext)
 
- // const {student} = useContext(AuthContext)
-
-const {data:lecture, isLoading, error} = useQuery(
-  ['lecture', props.lId],
-  () => { if (props.lId) return getLectureById(props.lId);
-  }
-)
+  const {
+    data: lecture,
+    isLoading,
+    error,
+  } = useQuery(['lecture', props.lId], () => {
+    if (props.lId) return getLectureById(props.lId);
+  });
 
   if (error) {
     alert('error bang');
@@ -36,7 +36,9 @@ const {data:lecture, isLoading, error} = useQuery(
   return (
     <div className="container mx-auto p-6">
       <div>
-        <QuestionnaireTable lecture={lecture} /**grade={grades[0]}*/></QuestionnaireTable>
+        <QuestionnaireTable
+          lecture={lecture} /**grade={grades[0]}*/
+        ></QuestionnaireTable>
       </div>
     </div>
   );
